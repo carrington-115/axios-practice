22378b828642d56dbfb7aa2044f571ee740a46e6
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-13 11:52:10 +0530

    installed nodemon and dotenv created a server route to make api calls with axios from a course api

--- README.md
+++ README.md
@@ -4,4 +4,16 @@
 
 ### Introduction
 
--
+- To install `axios` we use `npm install axios`
+- Axios has a simple syntax, it is just `axios.method(url)`. If the method is not passed, `axios(url)` then the default method is get
+
+```js
+import axios from "axios";
+axios.get(url);
+axios.post(url);
+
+// axios.method(url)
+axios(url); // default is get
+```
+
+- Since axios returns a promise, we can use async-await.
--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(process.env.API_URL);
+    console.log(response);
+  } catch (error) {
+    return res.end(error);
+  }
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`The server is running on ${process.env.PORT}`)
+);
