92b635860497111bedd5a1b967ca9d414ec5c893
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-13 16:12:19 +0530

    learned to use axios for get and post requests

--- src/app.js
+++ src/app.js
@@ -4,11 +4,29 @@ const app = express();
 const axios = require("axios");
 
 app.use(express.json());
+app.use(express.static("./views"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req, res) => {
+app.get("/courses", async (req, res) => {
   const { data } = await axios.get(process.env.API_URL);
-  console.log(data);
+  if (!data) throw Error(data.response);
   res.json(data);
+});
+
+// using get with headers
+app.get("/get-with-headers", async (req, res) => {
+  const { data } = await axios.get(process.env.API_URL, {
+    Headers: {
+      Accept: "application/json",
+    },
+  });
+  console.log(data);
+});
+
+app.post("/user-profile", (req, res) => {
+  const data = req.body;
+  if (!data) return res.end("Invalid data");
+  res.json({ success: true, data: data });
 });
 
 app.listen(process.env.PORT, () =>
